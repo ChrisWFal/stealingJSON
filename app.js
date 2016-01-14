0697824c3f5cdab639f3de7ba41460e536eac9cb
Chris Falkenberg <falkenbergch@mullenmail.com>
2016-01-14 11:46:02 -0700

    new API

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$http', function ($scope, $http) 
+myApp.controller('mainController', ['$scope', '$http', function ($scope, $http)
     {
-    $http.get('https://data.hartford.gov/api/views/889t-nwfu/rows.json?accessType=DOWNLOAD')
-        .success(function (result) 
-            {
-            $scope.complaints = result;
-            console.log("Hooray!");
+        $http.get('http://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=c9796fd40fb6ade46c14b7f31e958378%3A1%3A74022479
+')
+            .success(function (result) {
+                var counter = 0;
+                $scope.complaints = result;
+                console.log("Hooray!");
+                $scope.causeOfDeath = [];
+
+
+
+                for (var i = 0; i < 22; i++) {
+                    var disease = result.data[i][11];
+                    //$scope.causeOfDeath.push(disease.substring(0,4));
+                    if ($scope.causeOfDeath.push(disease.substring(0, 4)) === "INFL") {
+                        counter ++;
+                    }
+
+                }
+            console.log(counter);
             })
-        .error(function (data, status) 
-            {
-            console.log("Oops...");
-            });  
+            .error(function (data, status) {
+                console.log("Oops...");
+            });
     }]);
